02c92bd67f8d5cbc47bbfc0363234a8799e13541
伊織 鈴木 <k022g1078@g.neec.ac.jp>
2023-05-29 15:59:31 +0900

    02_09EX課題

--- EnemyBullet.cpp
+++ EnemyBullet.cpp
@@ -4,11 +4,22 @@ void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector
 	assert(model);
 
 	model_ = model;
-	textureHandle_ = TextureManager::Load("./Resources/tex1.png");
+	textureHandle_ = TextureManager::Load("./Resources/red1x1.png");
 	worldTransform_.Initialize();
 	velosity_ = velosity;
 
 	worldTransform_.translation_ = position;
+
+	worldTransform_.scale_.x = 0.5f;
+	worldTransform_.scale_.y = 0.5f;
+	worldTransform_.scale_.z = 3.0f;
+
+	worldTransform_.rotation_.y = std::atan2f(velosity_.x, velosity_.z);
+
+	float velosityXZ = sqrtf((velosity_.x * velosity_.x) + (velosity_.z * velosity_.z));
+
+	worldTransform_.rotation_.x = std::atan2f(-velosity_.y, velosityXZ);
+
 }
 void EnemyBullet::Update() {
 
