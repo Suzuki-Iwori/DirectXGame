4e4693011a28d860b70c4d9143cba1e84826c88e
伊織 鈴木 <k022g1078@g.neec.ac.jp>
2023-07-10 14:23:22 +0900

    l

--- math/Vector3.cpp
+++ math/Vector3.cpp
@@ -51,9 +51,31 @@ Vector3 operator*(const Vector3& v, const float& f) {
 
 	return result *= f;
 }
+float Dot(const Vector3& v1, const Vector3& v2) {
+
+	return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+}
 Vector3 Normalize(const Vector3& vector) {
 
 	float length = sqrtf((vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z));
 
 	return {(vector.x / length), (vector.y / length), (vector.z / length)};
+}
+Vector3 Slarp(Vector3& velocity, const Vector3& toVelocity, const float& t) {
+
+	float angle = acos(Dot(velocity, toVelocity));
+
+	float sangle = sin(angle);
+
+	float pS = sin(angle * (1.0f - t));
+	float pE = sin(angle * t);
+
+	if (sangle != 0.0f) {
+		velocity.x = (pS * velocity.x + pE * toVelocity.x) / sangle;
+		velocity.y = (pS * velocity.y + pE * toVelocity.y) / sangle;
+		velocity.z = (pS * velocity.z + pE * toVelocity.z) / sangle;
+	}
+
+	return velocity;
+
 }
--- math/Vector3.h
+++ math/Vector3.h
@@ -15,4 +15,6 @@ Vector3& operator-=(Vector3& v1, const Vector3& v2);
 Vector3 operator-(const Vector3& v1, const Vector3& v2);
 Vector3& operator*=(Vector3& v, const float& f);
 Vector3 operator*(const Vector3& v, const float& f);
+float Dot(const Vector3& v1, const Vector3& v2);
 Vector3 Normalize(const Vector3& vector);
+Vector3 Slarp(Vector3& velocity, const Vector3& toVelocity, const float& t);
